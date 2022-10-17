8d75417f20ca701b268108264ebbb5ad23ee1aa6
lion-rion <lionact08@gmail.com>
2022-10-17 16:16:27 +0900

    🎉 add : migrations

---
+++ src/database/migrations/2022_10_16_112855_create_judgements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJudgementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('judgements', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('sql-1')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('judgements');
+    }
+}
