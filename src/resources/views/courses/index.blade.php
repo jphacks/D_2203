3450cb2656e2a134ae1428577114983ab16dac69
lion-rion <lionact08@gmail.com>
2022-10-19 02:11:31 +0900

    ➕ add : courses

---
+++ src/resources/views/courses/index.blade.php
@@ -0,0 +1,65 @@
+
+<head><link rel="stylesheet" href="{{asset('css/style.css')}}"></head>
+<body>
+    <div class="container">
+        <div class="row">
+            @if($judge->sql_1 == 0)
+            <a href="/courses/sql">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <div class="our-team">
+                    <div class="picture">
+                        <img class="img-fluid" src="https://budougumi0617.github.io/logos/mysql.png">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="course_name">SQLi</h3>
+                    </div>
+                    <div class="social">
+                        <p class="course_detail">データベースから不正にデータを取得することができてしまう脆弱性</p>
+                    </div>
+                </div>
+            </div>
+            </a>
+            @endif
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <div class="our-team">
+                    <div class="picture">
+                        <img class="img-fluid" src="https://budougumi0617.github.io/logos/mysql.png">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="course_name">SQLi</h3>
+                    </div>
+                    <div class="social">
+                        <p class="course_detail">データベースから不正にデータを取得することができてしまう脆弱性</p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <div class="our-team">
+                    <div class="picture">
+                        <img class="img-fluid" src="https://budougumi0617.github.io/logos/mysql.png">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="course_name">SQLi</h3>
+                    </div>
+                    <div class="social">
+                        <p class="course_detail">データベースから不正にデータを取得することができてしまう脆弱性</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <div class="our-team">
+                    <div class="picture">
+                        <img class="img-fluid" src="https://budougumi0617.github.io/logos/mysql.png">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="course_name">SQLi</h3>
+                    </div>
+                    <div class="social">
+                        <p class="course_detail">データベースから不正にデータを取得することができてしまう脆弱性</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
