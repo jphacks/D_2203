4dc09a601f1a51b787341dbbbced5a67237e12ea
lion-rion <lionact08@gmail.com>
2022-10-21 21:00:22 +0900

    ♻️ change : table

--- src/database/migrations/2022_10_16_112855_create_judgements_table.php
+++ src/database/migrations/2022_10_16_112855_create_judgements_table.php
@@ -15,7 +15,24 @@ class CreateJudgementsTable extends Migration
     {
         Schema::create('judgements', function (Blueprint $table) {
             $table->id();
+            $table->boolean('law_1')->default(True);
+            $table->boolean('law_1_clear')->default(false);
+            $table->boolean('law_2')->default(false);
+            $table->boolean('law_2_clear')->default(false);
+            $table->boolean('law_3')->default(false);
+            $table->boolean('law_3_clear')->default(false);
+            $table->boolean('xss_1')->default(false);
+            $table->boolean('xss_1_clear')->default(false);
+            $table->boolean('xss_2')->default(false);
+            $table->boolean('xss_2_clear')->default(false);
+            $table->boolean('xss_3')->default(false);
+            $table->boolean('xss_3_clear')->default(false);
             $table->boolean('sql_1')->default(false);
+            $table->boolean('sql_1_clear')->default(false);
+            $table->boolean('sql_2')->default(false);
+            $table->boolean('sql_2_clear')->default(false);
+            $table->boolean('sql_3')->default(false);
+            $table->boolean('sql_3_clear')->default(false);
             $table->timestamps();
         });
     }
