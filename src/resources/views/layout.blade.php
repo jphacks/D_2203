73c64ebd5829a4d9df906203067c2d2ebd651c7a
lion-rion <lionact08@gmail.com>
2022-10-22 13:42:29 +0900

    😶

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -4,6 +4,7 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta name="author" content="SemiColonWeb" />
         <title>@yield('title')</title>
+        
         <link rel="stylesheet" href="{{asset('css/style2.css')}}">
         <link href="https://fonts.googleapis.com/css?family=Economica:700" rel="stylesheet">
         <link rel="preload" href="https://fonts.googleapis.com/css?family=Lato:300,400,700|M+PLUS+Rounded+1c:300,400,500,700,800|Quicksand:400,700&amp;display=swap" as="style">    
